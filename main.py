1a8c77f787e570b750c9792ca38de9c71f171695
BrunCrvlh <bruno.carvalho@dcomp.ufs.br>
2026-09-01 15:35:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from cliente import criar_cliente
+from conta import criacao_de_conta, depositar, sacar
 
 # cliente
 nome, cpf = criar_cliente('Bruno', '12345678900')
 
 # conta
-numero, cliente, saldo =
+numero, cliente, saldo = criacao_de_contaUpdate 
 
 # deposito
 saldo = depositar(saldo, 500)
