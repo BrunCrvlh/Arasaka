c8f275218c25090fbbca6e2657062cc76613ca4f
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-01 00:14:49 -0300

    Refactor account creation and add banking functions

--- conta.py
+++ conta.py
@@ -1,15 +1,21 @@
-def criacao_de_conta(cliente):
-    numero_da_conta = 260078955
+def criacao_de_conta(cliente, mes_de_nascimento):
+    numero_da_conta = "2602" + (str(mes_de_nascimento)) + " - 55"
     nome = cliente
-    saldo = float(0)
-    conta = (numero_da_conta, nome, saldo)
+    saldo_atual = float(0)
     
-    return conta
+    return numero_da_conta, nome, saldo_atual
 
-cliente = "XXXXX"
-conta = criacao_de_conta(cliente)
-
-print("         Sua conta foi criada com sucesso!          ")
-print("Número da conta: ", conta[0])
-print("Cliente: ", conta[1])
-print("Saldo: ", conta[2])
+def consultar_saldo(saldo_atual):
+    return saldo_atual
+    
+def depositar(saldo_atual, valor_de_deposito):
+    if valor_de_deposito > 0:
+        return saldo_atual + valor_de_deposito
+    else:
+        return saldo_atual
+        
+def sacar(saldo_atual, valor_de_saque):
+    if valor_de_saque <= saldo_atual:
+        return saldo_atual - valor_de_saque
+    else:
+        return saldo_atual
