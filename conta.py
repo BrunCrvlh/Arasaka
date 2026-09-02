ac7f8ee636c7ceef46ba73e46f056d174e8a4b2b
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-02 00:06:25 -0300

    Refactor account creation function parameters

--- conta.py
+++ conta.py
@@ -1,5 +1,5 @@
-def criacao_de_conta(cliente, mes_de_nascimento):
-    numero_da_conta = "2602" + (str(mes_de_nascimento)) + "9 - 55"
+def criacao_de_conta(cliente):
+    numero_da_conta = "2602079 - 55"
     nome = cliente
     saldo_atual = float(0)
     
