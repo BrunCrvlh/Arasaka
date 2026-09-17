9e9f89692f8a0c8867b12411039bfabe07e3ee08
BrunCrvlh <bruno.carvalho@dcomp.ufs.br>
2026-09-17 14:31:21 -0300

    Update dados_no_json.py

--- dados_no_json.py
+++ dados_no_json.py
@@ -5,12 +5,9 @@ def salvar_dados_gerais_do_banco (clientes, agencias, contas):
         json.dump([clientes, agencias, contas], arquivo)
 
 def carrega_dados_gerais_do_banco():
-   try:
-        with open("dados.json", "r") as arquivo:
-            dados = json.load(arquivo)
-            clientes = [tuple(c) for c in dados[0]]
-            agencias = [tuple(a) for a in dados[1]]
-            contas = [tuple(c) for c in dados[2]]
-            return clientes, agencias, contas
-    except FileNotFoundError:
-        return [], [], []
+    with open("dados.json", "r") as arquivo:
+        dados = json.load(arquivo)
+        clientes = [tuple(c) for c in dados[0]]
+        agencias = [tuple(a) for a in dados[1]]
+        contas = [tuple(c) for c in dados[2]]
+        return clientes, agencias, contas
