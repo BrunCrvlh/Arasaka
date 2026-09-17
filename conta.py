3e810826bec8e1116c399c23288a0bcfab65be59
BrunCrvlh <bruno.carvalho@dcomp.ufs.br>
2026-09-17 00:29:55 -0300

    teste

--- conta.py
+++ conta.py
@@ -4,26 +4,17 @@ contas=[]
 proximo_numero_conta = 1
 
 #como o cpf ja identifica o cliente, eu optei por tirar o nome do cliente dessa funcao ok?
-def criacao_de_conta(cpf:
-    numero_da_conta = "2602079 - 55"
-    nome = cliente
-    login = "Bruno44"
-    senha = "12345"
-    saldo_atual = float(0)
+def criacao_de_conta(cpf, codigo_agencia, login, senha):
+    global proximo_numero_conta
     
-    return numero_da_conta, cpf, nome, login, senha, saldo_atual
+    nova_conta = (proximo_numero_conta, cpf_cliente, codigo_agencia, login, senha, 0.0)
+    contas.append(nova_conta)
+    proximo_numero_conta = proximo_numero_conta + 1
+    return nova_conta
 
-def consultar_saldo(saldo_atual):
-    return saldo_atual
-    
-def depositar(saldo_atual, valor_de_deposito):
-    if valor_de_deposito > 0:
-        return saldo_atual + valor_de_deposito
-    else:
-        return saldo_atual
-        
-def sacar(saldo_atual, valor_de_saque):
-    if valor_de_saque <= saldo_atual and valor_de_saque > 0:
-        return saldo_atual - valor_de_saque
-    else:
-        return saldo_atual
+def buscar_contar_por_numero(numero):
+    for conta in contas:
+        if conta[0] == numero:
+            return conta
+            return None
+
