4f961c34806a42e0ca7242992850a1ce7a20d947
BrunCrvlh <bruno.carvalho@dcomp.ufs.br>
2026-09-16 23:59:15 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,3 +1,12 @@
-def criar_cliente(nome, cpf):
-  cliente = nome
-  return cliente, cpf
+clientes = []
+
+def criar_client(nome,cpf):
+  if buscar_cliente_por_cpf(cpf) is not None: 
+    print("Já existe um cliente cadastrado com esse mesmo CPF")
+    return None
+
+novo_cliente = (nome,cpf)
+clientes.append(nome_cliente)
+return novo_cliente
+
+def buscar
