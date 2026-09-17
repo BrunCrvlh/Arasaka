cb4e3ea5553b54d581f632cf880717bc89bc02ce
BrunCrvlh <bruno.carvalho@dcomp.ufs.br>
2026-09-17 00:36:16 -0300

    teste denovo

--- conta.py
+++ conta.py
@@ -18,3 +18,28 @@ def buscar_contar_por_numero(numero):
             return conta
             return None
 
+def indice_da_conta(numero):
+    for indice in range (len(contas)):
+        if contas[indice][0] == numero:
+            return indice
+            return -1
+
+def listar_contas():
+    if len(contas) == 0:
+        print("Nenhuma conta cadastrada")
+        return
+        print("\n------- CONTAS -------")
+        for conta in contas:
+            numero = conta[0]
+            cpf = conta[1]
+            condigo_agencia= conta[2]
+            saldo = conta[5]
+            print("Conta:", numero, " CPF:", cpf, " Agência:", codigo_agencia, "Saldo: R$", Saldo)
+
+def consultar_saldo(numero_cliente):
+     conta = buscar_conta_por_numero(numero_conta)
+    if conta is None::
+        print("Conta nao encontrada")
+        return None
+        return conta[5]
+
