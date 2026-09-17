f965b10f370890cbddfd4253148a3858a933a02c
BrunCrvlh <bruno.carvalho@dcomp.ufs.br>
2026-09-17 01:35:41 -0300

    tudo corrigidinhum cuida

--- cliente.py
+++ cliente.py
@@ -2,17 +2,17 @@
 clientes = []
 
 #funcaozinha q verifica se o cpf ja foi usado pra criar algum cliente, e se o cpf ainda n foi utilizado, vai montar a tupla (nome,cpf) e adiciona na lista, e depois disso retorna a tupla pro main 
-def criar_client(nome,cpf):
+def criar_cliente(nome,cpf):
   if buscar_cliente_por_cpf(cpf) is not None: 
     print("Já existe um cliente cadastrado com esse mesmo CPF")
     return None 
-    
+
   novo_cliente = (nome,cpf)
-  clientes.append(nome_cliente)clientes.append(nome_cliente)
+  clientes.append(novo_cliente)
   return novo_cliente
 
 #percorre a lista bem na posicao que o cpf ta (posicao 1) e se encontrar, ele devolve a tupla daquele cpf
-def buscar_clientes_por_cpf(cpf): 
+def buscar_cliente_por_cpf(cpf): 
   for cliente in clientes:
     if cliente[1] == cpf:
       return cliente
@@ -24,7 +24,7 @@ def listar_clientes ():
     print("Nenhum cliente cadastrado.")
     return
     
-  print("n------- CLIENTES -------")
+  print("\n------- CLIENTES -------")
   for cliente in clientes: 
       nome = cliente[0]
       cpf = cliente[1]
