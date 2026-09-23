bd739687690ad4ab5351fde4a16ebe4f90009392
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-22 23:51:23 -0300

    Fix formatting of function definition

--- dados_no_json.py
+++ dados_no_json.py
@@ -1,6 +1,6 @@
 import json
 
-def salvar_dados_gerais_do_banco (clientes, agencias, contas):
+def salvar_dados_gerais_do_banco(clientes, agencias, contas):
    with open("dados.json", "w") as arquivo:
         json.dump([clientes, agencias, contas], arquivo)
 
