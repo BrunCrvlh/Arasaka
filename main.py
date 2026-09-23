48b250e48a1aab7eeca12cc446f3c0a3ecd7b898
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-23 10:46:28 -0300

    Fix indentation and remove unused variable assignments

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from menu import menu
-
-clientes_salvos, agencias_salvas, contas_salvas = carrega_dados_gerais_do_banco()
-definir_clientes(clientes_salvos)
-definir_agencias(agencias_salvas)
-definir_contas(contas_salvas)
             
 menu()
 
