8c5475b2d1d70cd9449a15e18459ba388a0edb2e
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-23 10:46:00 -0300

    Initialize saved data in menu.py
    Load saved data for clients, agencies, and accounts.

--- menu.py
+++ menu.py
@@ -6,6 +6,11 @@ from agencia import (criar_agencia, listar_agencias, buscar_agencia_por_codigo, 
 
 from dados_no_json import (salvar_dados_gerais_do_banco, carrega_dados_gerais_do_banco)
 
+clientes_salvos, agencias_salvas, contas_salvas = carrega_dados_gerais_do_banco()
+definir_clientes(clientes_salvos)
+definir_agencias(agencias_salvas)
+definir_contas(contas_salvas)
+
 def menu():
     opcao = ""
     while opcao != "0": 
