57af47019ec6a6837ec72153aecc513c186c7657
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-24 10:18:52 -0300

    Fix import statement for salvar_dados_gerais_do_banco

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from menu import menu
-from dados_no_json.py import salvar_dados_gerais_do_banco
+from dados_no_json import salvar_dados_gerais_do_banco
             
 menu()
 
-print("Sessão encerrada.")            
-salvar_dados_gerais_do_banco(clientes, agencias, contas)
+print("Sessão encerrada.")
