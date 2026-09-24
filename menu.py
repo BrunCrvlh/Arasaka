9204e1dbd6490a86d146bc6b5215a2d31070aa5f
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-24 10:18:07 -0300

    Update account creation prompts for login and password

--- menu.py
+++ menu.py
@@ -55,8 +55,10 @@ def menu():
 
             elif agencia is None:
                 print("Agência não encontrada!")
-            login = input("Login: ")
-            senha = input("Senha: ")
+
+            print('Crie sua conta: ')
+            login = input("Seu login: ")
+            senha = input("Sua senha: ")
 
             conta = criacao_de_conta(cpf, codigo_agencia, login, senha)
             print("Conta criada com sucesso!")
