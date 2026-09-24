2da5646a27dc1b73da4b9e2229a4e9f557b9db3f
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-24 12:44:26 -0300

    Import functions for client, agency, and account setup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from menu import menu
+from cliente import definir_clientes
+from agencia import definir_agencias
+from conta import definir_contas
 from dados_no_json import (salvar_dados_gerais_do_banco, carrega_dados_gerais_do_banco)
 
 clientes_salvos, agencias_salvas, contas_salvas = carrega_dados_gerais_do_banco()
