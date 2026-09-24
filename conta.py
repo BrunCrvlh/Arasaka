5dc0032f4604d0d70ffb932c24663bba2e1c888e
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-23 23:46:41 -0300

    Change parameter name from 'cpf' to 'cpfs'

--- conta.py
+++ conta.py
@@ -5,10 +5,10 @@ proximo_numero_conta = 1
 
 #como o cpf ja identifica o cliente, eu optei por tirar o nome do cliente dessa funcao ok
 #nao tenho muito q falar sobre essa funcao, é a mesma de antes mas alterada, tem o global ali pra poder modificar quem ta fora da funcao, monta a tupla e tem o saldo inicial de 1000.0
-def criacao_de_conta(cpf, codigo_agencia, login, senha):
+def criacao_de_conta(cpfs, codigo_agencia, login, senha):
     global proximo_numero_conta
     
-    nova_conta = (proximo_numero_conta, cpf, codigo_agencia, login, senha, 0.0)
+    nova_conta = (proximo_numero_conta, cpfs, codigo_agencia, login, senha, 0.0)
     contas.append(nova_conta)
     proximo_numero_conta = proximo_numero_conta + 1
     return nova_conta
