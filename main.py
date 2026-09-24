68e12f6584ac838cfab1910ddafdcc0743032bf7
rikelmegs <rikelme.dossantos@dcomp.ufs.br>
2026-09-24 11:29:14 -0300

    Load saved data for clients, agencies, and accounts

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from menu import menu
 from dados_no_json import (salvar_dados_gerais_do_banco, carrega_dados_gerais_do_banco)
+
+clientes_salvos, agencias_salvas, contas_salvas = carrega_dados_gerais_do_banco()
+definir_clientes(clientes_salvos)
+definir_agencias(agencias_salvas)
+definir_contas(contas_salvas)
             
 menu()
 
